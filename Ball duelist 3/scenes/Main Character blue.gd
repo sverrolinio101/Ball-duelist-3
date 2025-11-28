621b0a6eccb215701ff64b78a228ea5fdb5e0436
sverrolinio101 <sverrevanderzwaan@gmail.com>
2025-11-28 13:42:02 +0100

    Add files via upload

--- Ball duelist 3/scenes/Main Character blue.gd
+++ Ball duelist 3/scenes/Main Character blue.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -635.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
